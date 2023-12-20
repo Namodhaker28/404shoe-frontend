9619f293c1df886114adf5140a1a92ff1af35dc5
Namo Narayan <namonarayan@Namos-MacBook-Pro.local>
2023-12-20 22:27:31 +0530

    bug fixes

--- context/context.js
+++ context/context.js
@@ -1,4 +1,5 @@
 import { fetchDataFromApi } from "@/utils/api";
+import Cookies from "js-cookie";
 import { createContext, useEffect, useState } from "react";
 
 const UserContext = createContext();
@@ -9,13 +10,21 @@ export const UserContextProvider = ({ children }) => {
   const getUser = async ()=>{
     try {
       const res = await fetchDataFromApi("me")
-    setUser(res)
+      setUser(res)
     } catch (error) {
-      console.log(error)
+      refresh()
+      console.log("error in me api",error)
+    }
+  }
+
+  const refresh = async ()=>{
+    try {
       const res = await fetchDataFromApi("refresh")
       Cookies.set("404Token", res?.accessToken);
+    } catch (error) {
+      Cookies.remove("404Token");
+      console.log("error in  refresh", error);
     }
-
   }
 
   useEffect(() =>{
