de5597e3f4c47e581796cd3a7dc64bef98ab3aa1
Namo Narayan <namonarayan@Namos-MacBook-Pro.local>
2024-01-05 08:54:35 +0530

    backend url chage

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,3 @@
 export const STRAPI_API_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
-export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api/v1/";
+export const API_URL =  "https://sneakerhub-9lxn.onrender.com/api/v1/";
